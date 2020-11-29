301aa76a245404266183014114fe80eee9302141
Daniel Silva <danfilipesilva@gmail.com>
2020-11-29 02:39:25 +0000

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -7,6 +7,10 @@ sudo apt-get update
 sudo apt-get install -y esl-erlang
 sudo apt-get install -y elixir
 
+# Install Phoenix framework
+yes | mix local.hex
+yes | mix archive.install hex phx_new
+
 # Start a Postgres container
 docker run --name some-postgres -e POSTGRES_PASSWORD=postgres -p 5432:5432 -d postgres
 
