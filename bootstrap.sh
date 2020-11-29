0959641d142127b9bbcf79b11a12c55b5a9b53c1
Daniel Silva <danfilipesilva@gmail.com>
2020-11-29 00:39:51 +0000

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,11 +4,11 @@
 wget https://packages.erlang-solutions.com/erlang-solutions_2.0_all.deb && sudo dpkg -i erlang-solutions_2.0_all.deb
 rm erlang-solutions_2.0_all.deb
 sudo apt-get update
-sudo apt-get install esl-erlang
-sudo apt-get install elixir
+sudo apt-get install -y esl-erlang
+sudo apt-get install -y elixir
 
 # Start a Postgres container
 docker run --name some-postgres -e POSTGRES_PASSWORD=postgres -p 5432:5432 -d postgres
 
 # Install inotify-tools
-sudo apt-get install inotify-tools
+sudo apt-get install -y inotify-tools
